fb6dc81174a84465413bc6f45dd0a2f133f1ef61
Alexis Love <lexiraeannaa@gmail.com>
2019-04-11 20:02:52 -0400

    Initial commit

---
+++ EnhanceQuizStarter/ViewController.swift
@@ -0,0 +1,136 @@
+//
+//  ViewController.swift
+//  EnhanceQuizStarter
+//
+//  Created by Pasan Premaratne on 3/12/18.
+//  Copyright © 2018 Treehouse. All rights reserved.
+//
+
+import UIKit
+import GameKit
+import AudioToolbox
+
+class ViewController: UIViewController {
+    
+    // MARK: - Properties
+    
+    let questionsPerRound = 4
+    var questionsAsked = 0
+    var correctQuestions = 0
+    var indexOfSelectedQuestion = 0
+    
+    var gameSound: SystemSoundID = 0
+    
+    let trivia: [[String : String]] = [
+        ["Question": "Only female koalas can whistle", "Answer": "False"],
+        ["Question": "Blue whales are technically whales", "Answer": "True"],
+        ["Question": "Camels are cannibalistic", "Answer": "False"],
+        ["Question": "All ducks are birds", "Answer": "True"]
+    ]
+    
+    // MARK: - Outlets
+    
+    @IBOutlet weak var questionField: UILabel!
+    @IBOutlet weak var Option1: UIButton!
+    @IBOutlet weak var Option2: UIButton!
+    @IBOutlet weak var Option3: UIButton!
+    @IBOutlet weak var Option4: UIButton!
+    @IBOutlet weak var playAgainButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        loadGameStartSound()
+        playGameStartSound()
+        displayQuestion()
+    }
+    
+    // MARK: - Helpers
+    
+    func loadGameStartSound() {
+        let path = Bundle.main.path(forResource: "GameSound", ofType: "wav")
+        let soundUrl = URL(fileURLWithPath: path!)
+        AudioServicesCreateSystemSoundID(soundUrl as CFURL, &gameSound)
+    }
+    
+    func playGameStartSound() {
+        AudioServicesPlaySystemSound(gameSound)
+    }
+    
+    func displayQuestion() {
+        indexOfSelectedQuestion = GKRandomSource.sharedRandom().nextInt(upperBound: trivia.count)
+        let questionDictionary = trivia[indexOfSelectedQuestion]
+        questionField.text = questionDictionary["Question"]
+        playAgainButton.isHidden = true
+    }
+    
+    func displayScore() {
+        // Hide the answer uttons
+        Option1.isHidden = true
+        Option2.isHidden = true
+        Option3.isHidden = true
+        Option4.isHidden = true
+        
+        // Display play again button
+        playAgainButton.isHidden = false
+        
+        questionField.text = "Way to go!\nYou got \(correctQuestions) out of \(questionsPerRound) correct!"
+    }
+    
+    func nextRound() {
+        if questionsAsked == questionsPerRound {
+            // Game is over
+            displayScore()
+        } else {
+            // Continue game
+            displayQuestion()
+        }
+    }
+    
+    func loadNextRound(delay seconds: Int) {
+        // Converts a delay in seconds to nanoseconds as signed 64 bit integer
+        let delay = Int64(NSEC_PER_SEC * UInt64(seconds))
+        // Calculates a time value to execute the method given current time and delay
+        let dispatchTime = DispatchTime.now() + Double(delay) / Double(NSEC_PER_SEC)
+        
+        // Executes the nextRound method at the dispatch time on the main queue
+        DispatchQueue.main.asyncAfter(deadline: dispatchTime) {
+            self.nextRound()
+        }
+    }
+    
+    // MARK: - Actions
+    
+    @IBAction func checkAnswer(_ sender: UIButton) {
+        // Increment the questions asked counter
+        questionsAsked += 1
+        
+        let selectedQuestionDict = trivia[indexOfSelectedQuestion]
+        let correctAnswer = selectedQuestionDict["Answer"]
+        
+        if (sender === Option1 &&  correctAnswer == "True") || (sender === Option2 && correctAnswer == "False") {
+            correctQuestions += 1
+            questionField.text = "Correct!"
+        } else {
+            questionField.text = "Sorry, wrong answer!"
+        }
+        
+        loadNextRound(delay: 2)
+    }
+    
+    
+    @IBAction func playAgain(_ sender: UIButton) {
+        // Show the answer buttons
+        Option1.isHidden = false
+        Option2.isHidden = false
+        Option3.isHidden = false
+        Option4.isHidden = false
+        
+        questionsAsked = 0
+        correctQuestions = 0
+        nextRound()
+    }
+    
+    
+}
+
